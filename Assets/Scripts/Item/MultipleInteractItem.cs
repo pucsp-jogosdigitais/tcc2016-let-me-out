32ad257f1af396c2e78c1c8086864d7d47e00ad1
Gabriel Spina <gtspina.dev@gmail.com>
2016-11-18 15:12:39 -0200

    armario quarto multiplas anim test

---
+++ Assets/Scripts/Item/MultipleInteractItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MultipleInteractItem: MonoBehaviour, IItem
+{
+    public Animator anim;
+    public string animTrigger = "interact";
+
+    public void Interact()
+    {
+        Debug.Log("interagiu");
+    }
+}
