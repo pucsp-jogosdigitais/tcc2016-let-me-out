059fc7fed8e4df697fa4bc5470ea1cb66f9a2e43
Gabriel Spina <gtspina.dev@gmail.com>
2016-11-25 18:42:35 -0200

    exclusao scripts

--- Assets/Scripts/GameInfo.cs
+++ Assets/Scripts/GameInfo.cs
@@ -6,14 +6,6 @@ public static class GameInfo {
 
 	public static bool paused = false;
 
-	public static KeyCode forward = KeyCode.W;
-	public static KeyCode backward = KeyCode.S;
-	public static KeyCode left = KeyCode.A;
-	public static KeyCode right = KeyCode.D;
-	public static KeyCode light = KeyCode.F;
-
 	public static float mouseSensivity = 2;
 	public static float volumeEffects = 1;
-
-    public static float brightness = 0;
 }
